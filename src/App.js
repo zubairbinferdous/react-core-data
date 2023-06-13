3963ddcad053ce0c6ec8b813fd7db5f626451f7e
zubairbinferdous <zubidev71@gmail.com>
2023-06-13 06:32:55 -0700

    ok

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Counter></Counter> */}
+      <User></User>
     </div>
   );
 }
 
+function User() {
+  const [person , setPerson] = useState([]);
+
+  useEffect( ()=>{
+
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => setPerson(data))
+
+  } , []);
+
+  
+  return(
+    <div className="">
+      <h1>user : {person.length}</h1>
+      {
+        person.map(data => <UserData name = {data.name} email= {data.email}></UserData>)
+      }
+    </div>
+  )
+}
+
+function UserData(props) {
+  return(
+    <div className="" style={{padding:'15px' , border:"3px solid red" , margin:'10px'}}>
+      <h1>user Name : {props.name}</h1>
+      <p>user Email : {props.email}</p>
+    </div>
+  )
+}
+
+// function Counter() {
+
+//   const [num , setNum] = useState(0);
+//   const increase = () => setNum(num + 1 )
+//   const decrease = () => setNum(num - 1 )
+
+//   return(
+//     <div className="">
+//         <h1>count : {num} </h1>
+//         <button onClick={increase}>increase</button>
+//         <button onClick={decrease}>decrease</button>
+//     </div>
+//   )
+// }
+
 export default App;
+// Fira Code, Operator Mono
+
+
